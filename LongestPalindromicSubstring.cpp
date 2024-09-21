c9d9c9a7f2bd2cc84e2460ccc443f6f2fa166b06
Allen_Paul <163329337+AllenPaul-2005@users.noreply.github.com>
2024-09-21 11:49:37 +0530

    Add files via upload

---
+++ LongestPalindromicSubstring.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+string expandAroundCenter(string s, int left, int right)
+{
+    while (left >= 0 && right < s.length() && s[left] == s[right])
+    {
+        left--;
+        right++;
+    }
+    return s.substr(left + 1, right - left - 1);
+}
+string longestPalindrome(string s)
+{
+    if (s.empty())
+        return "";
+
+    string longest = "";
+    for (int i = 0; i < s.length(); i++)
+    {
+        // Check for odd-length palindrome
+        string oddPalindrome = expandAroundCenter(s, i, i);
+        if (oddPalindrome.length() > longest.length())
+        {
+            longest = oddPalindrome;
+        }
+
+        // Check for even-length palindrome
+        string evenPalindrome = expandAroundCenter(s, i, i + 1);
+        if (evenPalindrome.length() > longest.length())
+        {
+            longest = evenPalindrome;
+        }
+    }
+    return longest;
+}
+
+int main()
+{
+    string s = "babad";
+    cout << "Longest Palindromic Substring: " << longestPalindrome(s) << endl;
+    return 0;
+}
